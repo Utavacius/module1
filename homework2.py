8c1e9d4d5d6f0cb537d7cf5c743057b7c24003a9
Utavacius <avernus1807@gmail.com>
2024-06-07 16:38:50 +0300

    1-4

---
+++ homework2.py
@@ -0,0 +1,5 @@
+homeworks_completed_count = 12
+hours_count = 1.5
+course_name = 'Python'
+time_for_a_single_homework = hours_count / homeworks_completed_count
+print(f'Курс: {course_name}, всего задач:{homeworks_completed_count}, затрачено часов: {hours_count}, среднее время выполнения {time_for_a_single_homework} часа.')
---
+++ homework3.py
@@ -0,0 +1,11 @@
+name = 'Ivan'
+print(f'Name: {name}')
+age = 27
+print(f'Age: {age}')
+age += 1
+print(f'New Age: {age}')
+is_student = True
+print(f'Is Student: {is_student}')
+
+
+
