df33da28901b1f7b27373b79d7888483b373a8b9
Utavacius <avernus1807@gmail.com>
2024-06-07 17:29:58 +0300

    homework5

---
+++ homework5.py
@@ -0,0 +1,5 @@
+immutable_var = 9, False, 'Cat', [5, 18]
+print(f'Immutable tuple: {immutable_var}')
+mutable_list = ['Dog', 'Parrot', 'Kitty']
+mutable_list[0] = 'Mouse'
+print(f'Mutable list: {mutable_list}')
